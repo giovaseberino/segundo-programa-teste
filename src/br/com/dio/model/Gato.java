96d0a153a37c0d2a25c633bbbaef18ef4ae3c0e4
Giovane Seberino <gseberino@gmail.com>
2022-06-27 14:13:39 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,45 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public Gato(String nome) {
+        this.nome = nome;
+    }
+
+    public Gato() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
